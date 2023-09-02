37eeb85698ad29b55d7e9cad60be8b4570829f9a
PixelBoot <bootingpixels@gmail.com>
2023-09-02 23:30:59 +0200

    taimen: Bringup PixelBuilds

--- aosp_taimen.mk
+++ aosp_taimen.mk
@@ -21,7 +21,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 # Inherit some common PixelExperience stuff.
 TARGET_BOOT_ANIMATION_RES := 1440
 TARGET_GAPPS_ARCH := arm64
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/pb/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/google/taimen/device.mk)
